4d4ecbab8da1f0db510b6f786daf2b115cb9f882
Jonathan Bayer <linuxgurugamer@gmail.com>
2017-05-28 15:14:25 -0400

    Updated for 1.3

--- TacSelfDestruct/buildRelease.bat
+++ TacSelfDestruct/buildRelease.bat
@@ -1,29 +1,13 @@
+
 @echo off
 
 copy /Y "bin\Release\TacSelfDestruct.dll" "..\GameData\TacSelfDestruct\Plugins"
 copy /Y TacSelfDestruct.version ..\GameData\TacSelfDestruct
 copy /Y ..\..\MiniAVC.dll ..\GameData\TacSelfDestruct
 
-set DEFHOMEDRIVE=d:
-set DEFHOMEDIR=%DEFHOMEDRIVE%%HOMEPATH%
-set HOMEDIR=
-set HOMEDRIVE=%CD:~0,2%
-
 set RELEASEDIR=d:\Users\jbb\release
 set ZIP="c:\Program Files\7-zip\7z.exe"
-echo Default homedir: %DEFHOMEDIR%
 
-rem set /p HOMEDIR= "Enter Home directory, or <CR> for default: "
-
-if "%HOMEDIR%" == "" (
-set HOMEDIR=%DEFHOMEDIR%
-) 
-echo %HOMEDIR%
-
-SET _test=%HOMEDIR:~1,1%
-if "%_test%" == ":" (
-set HOMEDRIVE=%HOMEDIR:~0,2%
-)
 
 set VERSIONFILE=TacSelfDestruct.version
 
